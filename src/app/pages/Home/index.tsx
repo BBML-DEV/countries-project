6c61eec777b4c22928c865285fdde0a9c08b7e83
Paulo Vitor <pv.marques2010@hotmail.com>
2023-07-13 19:05:13 -0300

    feat: consumindo a API de dados dos países

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from 'react'
 import { Header } from '../../shared/components/Header'
 import { Inputs } from '../../shared/components/Inputs'
 import { List } from '../../shared/components/List'
 import { HomeContainer } from './styled'
 
+export interface CoutriesDataProps {
+  id: number
+  name: string
+  flag: string
+  population: number
+  region: string
+  capital: string
+}
+
 export const Home = () => {
+  const [data, setData] = useState<Array<CoutriesDataProps>>([])
+
+  async function getApiData() {
+    const response = await fetch('https://restcountries.com/v2/all')
+    const json = await response.json()
+    setData(json)
+  }
+
+  console.log(data)
+
+  useEffect(() => {
+    getApiData()
+  }, [])
+
   return (
     <HomeContainer>
       <Header />
       <Inputs />
-      <List />
+      <List data={data} />
     </HomeContainer>
   )
 }
