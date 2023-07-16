0b42cd6279f8b7924fc3d82cf9dfd3d41d7e12db
Paulo Vitor <pv.marques2010@hotmail.com>
2023-07-16 19:34:52 -0300

    feat: adicionando o componente de singleCard as rotas

--- src/app/shared/router/index.tsx
+++ src/app/shared/router/index.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../../pages/Home'
 import { DefaultLayout } from '../../layout/defaultLayout'
+import SingleCard from '../../pages/SingleCard'
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/post/:number" element={<Post />} /> */}
+        <Route path="/singleCard" element={<SingleCard />} />
       </Route>
     </Routes>
   )
