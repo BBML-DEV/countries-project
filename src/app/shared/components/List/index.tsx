cf458102324ac2b03aea69aa5fde695f17854137
Paulo Vitor <pv.marques2010@hotmail.com>
2023-07-16 00:34:04 -0300

    refactor: melhorando a lógica de captura dos valores selecionados através do input select

--- src/app/shared/components/List/index.tsx
+++ src/app/shared/components/List/index.tsx
@@ -6,20 +6,27 @@ type DataPros = {
   data: CoutriesDataProps[]
   optionValue: string
 }
+
+enum ContinentOptions {
+  Asia = 'Asia',
+  Americas = 'Americas',
+  Africa = 'Africa',
+  Europe = 'Europe',
+  Oceania = 'Oceania',
+}
+
+const isOptionValid = (option: string): option is ContinentOptions => {
+  return Object.values(ContinentOptions).includes(option as ContinentOptions)
+}
+
 export const List = ({ data, optionValue }: DataPros) => {
   const filterListOfRegions = data.filter((item) => item.region === optionValue)
 
-  console.log(filterListOfRegions)
-
   return (
     <ListContainer className="container">
-      {optionValue.length > 0
-        ? filterListOfRegions.map((item) => {
-            return <Card key={item.id} data={item} />
-          })
-        : data.map((item) => {
-            return <Card key={item.id} data={item} />
-          })}
+      {isOptionValid(optionValue)
+        ? filterListOfRegions.map((item) => <Card key={item.id} data={item} />)
+        : data.map((item) => <Card key={item.id} data={item} />)}
     </ListContainer>
   )
 }
